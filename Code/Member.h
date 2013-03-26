bf54f8e718169b5f277bb3f9280541ae1d61ab33
Verdi R-D <verdi@azend.org>
2013-03-26 15:59:23 -0400

    Added Member class as per design requirements

---
+++ Code/Member.h
@@ -0,0 +1,48 @@
+
+#ifndef _MEMBER_H
+#define _MEMBER_H
+
+/**
+ * 
+ * @file Member.h
+ *
+ * @brief This file holds Member class.
+ *
+ */
+
+#include <iostream>
+
+#define IBUTTON_BYTES /**< The number of bytes in an iButton address */
+
+/**
+ *
+ * @class Member
+ * @brief This class models a member
+ *
+ */
+class Member {
+
+	private:
+		unsigned char ibuttonAddr[IBUTTON_BYTES];
+		std::string firstName;
+		std::string lastName;
+
+	public:
+		/* Constructors / Deconstructors */
+		Member();
+		~Member();
+
+		/* Accessors */
+		unsigned char * GetiButtonAddr();
+		std::string GetFirstName();
+		std::string GetLastName();
+
+		/* Mutators */
+		void SetiButtonAddr ( unsigned char newAddr[IBUTTON_BYTES] );
+		void SetFirstName ( std::string newFirstName );
+		void SetLastName ( std::string newLastName );
+
+
+};
+
+#endif
