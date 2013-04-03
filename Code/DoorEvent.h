adb0c41681527e3897844c6d8f7ead39c3077fcf
icampb01 <icampb01@gmail.com>
2013-04-03 13:41:49 -0400

    adding some UML diagrams to da hub
    fuck ya

--- Code/DoorEvent.h
+++ Code/DoorEvent.h
@@ -1,6 +1,32 @@
 #ifndef _DOOREVENT_H_
 #define _DOOREVENT_H_
+///
+///\
+///
+///
+///
+///
+///
+#include<iostream>
+#include<ctime>
 
-
+class DoorEvent {
+private:
+    unsigned char accessCode[8];
+    int status;
+    time_t currentTime;
+    
+public:
+    //constructor
+    DoorEvent()
+    //mutators
+    void setAccessCode(unsigned char[] newAccessCode);
+    void setStatus(int newStatus);
+    void setTime(time_t newCurrentTime);
+    //accessors
+    unsigned char getAccessCode(void);
+    int getStatus(void);
+    time_t getTime(void);
+};
 
 #endif
