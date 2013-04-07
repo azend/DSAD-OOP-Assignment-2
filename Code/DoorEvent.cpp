5b6965bf6fcb3236a98390d46ef988bee1d462da
icampb01 <icampb01@gmail.com>
2013-04-07 16:44:23 -0400

    finished DoorEvent.h & DoorEvent.cpp
    changed time to a string H M S and added the ability to write the door
    event to a log file.

--- Code/DoorEvent.cpp
+++ Code/DoorEvent.cpp
@@ -4,31 +4,141 @@
 
 #include "DoorEvent.h"
 
-DoorEvent::DoorEvent(){
-    status = 0;
+///
+/// \brief <b>Brief Description</b> - Brings in a new I button code
+/// \details <b>Details</b>
+///
+/// This method clears the old code and sets the new I button code. 
+///
+/// \return This method does not return anything.
+///
+
+void DoorEvent::SetAccessCode(char newAccessCode[]){
+    buttonAddr[0] = '\0';
+    strcpy(buttonAddr, newAccessCode);
 }
 
-void DoorEvent::SetAccessCode(unsigned char newAccessCode[]){
-    
-}
+///
+/// \brief <b>Brief Description</b> - This sets the status to pass or fail.
+///
+/// \return This method does not return anything.
+///
 
-void DoorEvent::SetStatus(int newStatus){
+void DoorEvent::SetStatus(bool newStatus){
     status = newStatus;
 }
 
+///
+/// \brief <b>Brief Description</b> - This method sets the current time
+/// \details <b>Details</b>
+///
+/// This method uses ctime library functions and structs to create a string containing the
+/// current time.
+///
+/// \return This method does not return anything.
+///
+
 void  DoorEvent::SetTime(time_t newCurrentTime){
-    
+    currentTime = newCurrentTime;
+    time(&currentTime);
+    timeyWimeyStuff = localtime(&currentTime);
+    strftime(timeString, sizeof(timeString), "%H:%M:%S", timeyWimeyStuff);
 }
 
-unsigned char DoorEvent::GetAccessCode(unsigned	char newAccessCode[]){
+///
+/// \brief <b>Brief Description</b> - This method gets the I button code from DoorEntry.
+///
+/// \return This method returns the I button code.
+///
+
+char DoorEvent::GetAccessCode(){
     return newAccessCode;
 }
+
+///
+/// \brief <b>Brief Description</b> - This method gets the status from DoorEntry.
+///
+/// \return This method returns the status.
+///
+
 
 int DoorEvent::GetStatus(){
     return status;
 }
 
-time_t DoorEvent::GetTime(){
-    return currentTime;
+///
+/// \brief <b>Brief Description</b> - This method gets the time string from DoorEntry.
+///
+/// \return This method returns the current time.
+///
+
+
+char DoorEvent::GetTime(){
+    return timeString;
 }
 
+///
+/// \brief <b>Brief Description</b> - This method writes entry data to the entry log
+/// \details <b>Details</b>
+///
+/// This method writes the I button code, whether or not the code passed or failed and the
+/// time to the entry log.  It also uses errorLevel to keep track of any wrong doings.
+///
+/// \return This method does not return anything.
+///
+
+
+void DoorEvent::writeToLog(char * path){
+    FILE * fp = NULL;
+    long fileSize = 0;
+    unsigned char * inputBuffer = {0};
+    int i = 0; // only include if you use a loop
+    
+    //get the size of the file that is about to be manipulated
+    if ((fp = fopen(path, "rb")) == NULL) {
+        errorLevel = 2;
+    }
+    
+    if (errorLevel == 0){
+        //get the size of the file and then point it back to the start
+        //bug!! will break if the file is bigger then 2gbs
+        fseek(fp, 0L, SEEK_END);
+        fileSize = ftell(fp);
+        fseek(fp, 0L, SEEK_SET);
+        fclose(fp);
+    }
+    
+    //open the file that you are working on
+    if (errorLevel == 0){
+        if ((fp = fopen(path, "ab")) == NULL){
+            errorLevel = 2;
+        }
+    }
+    
+    if (errorLevel == 0){
+        if ((inputBuffer = (unsigned char*) malloc((fileSize + 1) * sizeof(unsigned char))) == NULL){
+            //In case of malloc error, make sure you attempt to free up any resources you are using.
+            errorLevel = 8;
+            fclose(fp);
+        }
+        else if (errorLevel == 0){
+            if (fread(inputBuffer, 1, fileSize, fp) == (unsigned int) fileSize){
+                if (status == true){
+                    fprintf(fp, "%s Access Granted %s\n", buttonAddr, timeString);
+                }
+                else if (status == false){
+                    fprintf(fp, "%s Access Denied %s\n", buttonAddr, timeString);
+                }
+            }
+            else {
+                //In case of read error, make sure you attempt to free up any resources you are using.
+                errorLevel = 4;
+                free(inputBuffer);
+                fclose(fp);
+            }
+        }
+    }
+    
+    free(inputBuffer);
+    fclose(fp);
+}
--- Code/DoorEvent.h
+++ Code/DoorEvent.h
@@ -7,31 +7,58 @@
 ///
 ///
 ///\class DoorEvent
-///\brief This class creates an entry event stores the access code, whether or not it passed or failed and the time.
+///\brief <b>Brief Description</b>This class stores data from an entry event
+///\details <b>Details</b>
 ///
+/// This class store the data from an entry event, including: the access code,
+/// the status and the time
+///
+///\author<i>Ian Campbell & Verdi R-D</i>
 ///
 
-#include<iostream>
-#include<ctime>
+
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+#include <string.h>
 
 class DoorEvent {
 private:
-    unsigned char buttonAddr[IBUTTON_BYTES];
-    int status;
+    char buttonAddr[IBUTTON_BYTES];///<This is where the i button value is stored
+    bool status;///<indicates pass or fail
     time_t currentTime;
+    struct tm * timeyWimeyStuff
+    char timeString[9]///<string that holds the time
+    int errorLevel;///<for file IO errors
     
 public:
-    //constructor/destructor
-    DoorEvent()
-    //~DoorEvent()
+    //Constructor
+    ///
+	/// \brief <b>Brief Description</b> - This is the constructor
+	/// \details <b>Details</b>
+	///
+    /// This initializes everything to zero and sets the current time
+	///
+	/// \return As this is a <i>constructor</i> Nothing is returned from the constuctor
+	///
+    DoorEvent(){
+        errorLevel = 0;
+        status = false;
+        buttonAddr[0] = '\0';
+        time(&currentTime);
+        timeyWimeyStuff = localtime(&currentTime);
+        strftime(timeString, sizeof(timeString), "%H:%M:%S", timeyWimeyStuff);
+    }
     //mutators
-    void SetAccessCode(unsigned char newAccessCode[]);
+    void SetAccessCode(char newAccessCode[]);
     void SetStatus(int newStatus);
     void SetTime(time_t newCurrentTime);
     //accessors
-    unsigned char GetAccessCode(void);
-    int GetStatus(void);
-    time_t GetTime(void);
+    char GetAccessCode(void);
+    bool GetStatus(void);
+    char GetTime(void);
+    //write door event to log
+    void writeToLog(char * path);
 };
 
 #endif
